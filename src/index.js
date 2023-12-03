69ebc77687a380aa70ff14735ed1331a494eaa8a
katyalle <katyalekhman@gmail.com>
2023-12-03 11:59:26 +0200

    c

--- src/index.js
+++ src/index.js
@@ -1,61 +1,172 @@
-import { fetchBreeds, fetchCatByBreed } from "./cat-api";
-import './styles.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SlimSelect from 'slim-select'
-import 'slim-select/dist/slimselect.css';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const ref = {
-    selector: document.querySelector('.breed-select'),
-    divCatInfo: document.querySelector('.cat-info'),
-    loader: document.querySelector('.loader'),
-    error: document.querySelector('.error'),
-};
-const { selector, divCatInfo, loader, error } = ref;
+const searchForm = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery');
+const KEY = '39758171-454faa2ce1768c54f925ff819';
 
-loader.classList.replace('loader', 'is-hidden');
-error.classList.add('is-hidden');
-divCatInfo.classList.add('is-hidden');
+let query = '';
+let page = 1;
+let simpleLightBox = new SimpleLightbox('.gallery a');
+let allPagesLoaded = false;
+const perPage = 40;
 
-let arrBreedsId = [];
-fetchBreeds()
-.then(data => {
-    data.forEach(element => {
-        arrBreedsId.push({text: element.name, value: element.id});
-    });
-    new SlimSelect({
-        select: selector,
-        data: arrBreedsId
-    });
+searchForm.addEventListener('submit', onSearchForm);
+
+function renderGallery(images) {
+  if (!gallery) {
+    return;
+  }
+
+  const markup = images
+    .map(image => {
+      const { id, largeImageURL, webformatURL, tags, likes, views, comments, downloads, } = image;
+      return `
+        <a class="gallery__link" href="${largeImageURL}">
+          <div class="gallery-item" id="${id}">
+            <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item"><b>Likes</b>${likes}</p>
+              <p class="info-item"><b>Views</b>${views}</p>
+              <p class="info-item"><b>Comments</b>${comments}</p>
+              <p class="info-item"><b>Downloads</b>${downloads}</p>
+            </div>
+          </div>
+        </a>
+      `;
     })
-.catch(onFetchError);
+    .join('');
 
-selector.addEventListener('change', onSelectBreed);
+  gallery.insertAdjacentHTML('beforeend', markup);
 
-function onSelectBreed(event) {
-    loader.classList.replace('is-hidden', 'loader');
-    selector.classList.add('is-hidden');
-    divCatInfo.classList.add('is-hidden');
+  // const { height: cardHeight } = document
+  //   .querySelector('.gallery')
+  //   .firstElementChild.getBoundingClientRect();
 
-    const breedId = event.currentTarget.value;
-    fetchCatByBreed(breedId)
+  // window.scrollBy({
+  //   top: cardHeight * 2,
+  //   behavior: 'smooth',
+  // });
+}
+
+function onSearchForm(e) {
+  e.preventDefault();
+  page = 1;
+  query = e.currentTarget.elements.searchQuery.value.trim();
+  allPagesLoaded = false;
+  gallery.innerHTML = '';
+
+  if (query === '') {
+    Notiflix.Notify.failure(
+      'The search string cannot be empty. Please specify your search query.',
+    );
+    return;
+  }
+
+  fetchImages(query, page, perPage)
     .then(data => {
-        loader.classList.replace('loader', 'is-hidden');
-        selector.classList.remove('is-hidden');
-        const { url, breeds } = data[0];
-        divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`
-        divCatInfo.classList.remove('is-hidden');
+      if (data.totalHits === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.',
+        );
+      } else {
+        renderGallery(data.hits);
+        const totalPages = Math.ceil(data.totalHits / perPage);
+
+        if (page >= totalPages) {
+          allPagesLoaded = true;
+        }
+        simpleLightBox.refresh();
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      }
     })
-    .catch(onFetchError);
-};
-
-function onFetchError(error) {
-    selector.classList.remove('is-hidden');
-    loader.classList.replace('loader', 'is-hidden');
-
-    Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!', {
-        position: 'center-center',
-        timeout: 5000,
-        width: '400px',
-        fontSize: '24px'
+    .catch(error => console.log(error))
+    .finally(() => {
+      searchForm.reset();
     });
+}
+
+function onloadMore() {
+  if (allPagesLoaded) {
+    return;
+  }
+  page += 1;
+
+  fetchImages(query, page, perPage)
+    .then(data => {
+      if (data.hits.length === 0) {
+        allPagesLoaded = true;
+      } else {
+        renderGallery(data.hits);
+        simpleLightBox.refresh();
+
+        const totalPages = Math.ceil(data.totalHits / perPage);
+
+        if (page >= totalPages) {
+          allPagesLoaded = true;
+          Notiflix.Notify.failure(
+            "We're sorry, but you've reached the end of search results.",
+          );
+        }
+        
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+
+function checkIfEndOfPage() {
+  return (
+    window.innerHeight + window.scrollY >=
+    document.documentElement.scrollHeight
+  );
+}
+
+function showLoadMorePage() {
+  if (checkIfEndOfPage()) {
+    onloadMore();
+  }
+}
+
+window.addEventListener('scroll', showLoadMorePage);
+
+arrowTop.onclick = function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 };
+
+window.addEventListener('scroll', function () {
+  arrowTop.hidden = scrollY < document.documentElement.clientHeight;
+});
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+axios.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    Notiflix.Notify.failure('Something went wrong. Please try again later.');
+    return Promise.reject(error);
+  },
+);
+
+async function fetchImages(query, page, perPage) {
+  const response = await axios.get(
+    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+  );
+  return response.data;
+}
+
+SmoothScroll({
+    animationTime: 800,
+    stepSize: 75,
+    accelerationDelta: 30,
+    accelerationMax: 2,
+    keyboardSupport: true,
+    arrowScroll: 50,
+    pulseAlgorithm: true,
+    pulseScale: 4,
+    pulseNormalize: 1,
+    touchpadSupport: true,
+  })
